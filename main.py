85760a13b0f9a31b16ed3773d1d6a6bb50e5b3c6
Bruno Severino de Almeida Rocha <brunosdar1@gmail.com>
2025-08-04 19:10:55 -0300

    adicionado: função de saque

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import time
+
 menu = """
 Bem-vindo ao Banco Python!
 Escolha uma opção:
@@ -21,20 +21,40 @@ def depositar(valor):
     if valor > 0:
         saldo += valor
         extrato.append(f"Depósito: R$ {valor:.2f}\n") 
-        print(f"Depósito de R$ {valor:.2f} realizado com sucesso!")
-        time.sleep(1)
+        return f"Depósito de R$ {valor:.2f} realizado com sucesso!"
     else:
-        print("Valor de depósito inválido!")
+        return "Valor de depósito inválido!"
 
+def sacar(valor):
+    global saldo, extrato, numero_saques, LIMITE_SAQUES
+    
+    if valor <= 0: return "Valor de saque inválido! O valor deve ser maior que zero."    
+    if valor > saldo: return "Saldo insuficiente para realizar o saque."       
+    if valor > limite: return f"Valor de saque excede o limite de R$ {limite:.2f}."    
+    if numero_saques >= LIMITE_SAQUES: return f"Limite de saques diários atingido ({LIMITE_SAQUES} saques)."
+        
+    else:
+        saldo -= valor
+        extrato.append(f"Saque: R$ {valor:.2f}\n")
+        numero_saques += 1
+        return f"Saque de R$ {valor:.2f} realizado com sucesso!"
+    
 while True:
     opcao = input(menu)
     if opcao == "d".lower():
-        time.sleep(1)
-        valor_deposito = float(input("Informe o valor do depósito: R$ "))
-        depositar(valor_deposito)
-        
+        try:
+            valor_deposito = float(input("Informe o valor do depósito: R$ "))
+            print(depositar(valor_deposito))
+        except ValueError:
+            print("Erro: Valor inválido. Por favor, insira um número válido.")
+
     elif opcao == "s".lower():
-        pass
+        try:
+            valor_saque = float(input("Informe o valor do saque: R$ "))
+            print(sacar(valor_saque))
+        except ValueError:
+            print("Erro: Valor inválido. Por favor, insira um número válido.")
+
     elif opcao == "e".lower():
         pass
     elif opcao == "q".lower():
@@ -42,3 +62,4 @@ while True:
         break
     else:
         print("Opção inválida! Por favor, escolha uma opção válida do menu.")
+
